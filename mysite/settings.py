cc8d33d1e6401286001fc31f0bd9a76f0bbbb348
kali <kali@kali.kali>
2022-07-23 22:04:54 -0500

    deploy 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_mqmdlv@_we*t%27$&#2nm^*63thnh+ecv4vk7ibxt!m&6ain&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://test-task-drf.herokuapp.com/"]
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
